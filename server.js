b371f44e1ee210e87006a864ee01975c9fec6cca
matei <mateidr7@gmail.com>
2024-10-28 18:49:24 +0200

    ASd

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.use(json());
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", process.env.front_link);
 
   // Request methods you wish to allow
   res.setHeader(

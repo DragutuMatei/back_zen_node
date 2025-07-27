2217d8b12bfaa62ecbea389ee4f766591b9607ec
Matei Dragutu <mateidr7@gmail.com>
2025-07-27 22:40:00 +0300

    asd

--- src/listen/categories.js
+++ src/listen/categories.js
@@ -1,3 +1,203 @@
+// // import { db, storage } from "../../config_fire.js";
+// // import {
+// //   addDoc,
+// //   collection,
+// //   deleteDoc,
+// //   doc,
+// //   getDoc,
+// //   getDocs,
+// //   orderBy,
+// //   query,
+// //   where,
+// // } from "firebase/firestore";
+// // import {
+// //   ref,
+// //   getDownloadURL,
+// //   uploadBytesResumable,
+// //   uploadBytes,
+// // } from "firebase/storage";
+// // import { v4 as uuid } from "uuid";
+// // import { check } from "../auth/auth.js";
+
+// // function orderByField(array, field, ascending = true) {
+// //   return array.sort((a, b) => {
+// //     if (a[field] < b[field]) {
+// //       return ascending ? -1 : 1;
+// //     }
+// //     if (a[field] > b[field]) {
+// //       return ascending ? 1 : -1;
+// //     }
+// //     return 0;
+// //   });
+// // }
+// // const addListenCat = async (req, res) => {
+// //   const data = {
+// //     categoryTitle: req.body.categoryTitle,
+// //     backgroundImage: "",
+// //     order: req.body.order,
+// //     listenRoutines: [],
+// //   };
+
+// //   try {
+// //     const storageRef = ref(
+// //       storage,
+// //       `/listen/cat/${req.files["backgroundImage"].name}`
+// //     );
+// //     // var reader = new window.FileReader();
+// //     // reader.onload = function () {
+// //     //   var blob = window.dataURLtoBlob(reader.result);
+// //     // };
+
+// //     // reader.readAsDataURL(req.files["backgroundImage"]);
+
+// //     await uploadBytes(
+// //       storageRef,
+// //       req.files["backgroundImage"].data
+// //       // reader/
+// //     );
+
+// //     const url = await getDownloadURL(storageRef);
+// //     data["backgroundImage"] = url;
+// //     data["id"] = uuid();
+// //     const document = await addDoc(collection(db, "categorie_listen"), data);
+// //     res.status(200).json({ ok: true, id: document.id });
+// //   } catch (error) {
+// //     //console.log(error);
+// //     res.status(500).json({ ok: false, error });
+// //   }
+// // };
+
+// // const getAllListenCats = async (req, res) => {
+// //   let data = [];
+// //   let abonament = "";
+// //   try {
+// //     const [decodedToken, userId, user] = await check(req);
+// //     abonament = user.abonament;
+// //   } catch (error) {
+// //     abonament = false;
+// //   }
+// //   try {
+// //     const listenCats = await getDocs(
+// //       collection(db, "categorie_listen"),
+// //       orderBy("order", "desc")
+// //     );
+// //     listenCats.forEach((doc) => {
+// //       data.push({ uid: doc.id, ...doc.data() });
+// //     });
+
+// //     data = orderByField(data, "order", false);
+// //     console.log(abonament);
+
+// //     if (abonament != false && abonament != "") {
+// //       for (let i = 0; i < data.length; i++) {
+// //         for (let j = 0; j < data[i].listenRoutines.length; j++) {
+// //           data[i].listenRoutines[j].isLocked = false;
+// //         }
+// //       }
+// //     }
+// //     for (let i = 0; i < data.length; i++) {
+// //       data[i].listenRoutines = orderByField(
+// //         data[i].listenRoutines,
+// //         "time",
+// //         true
+// //       );
+// //       console.log(data[i].listenRoutines);
+// //     }
+// //     res.status(200).json({ ok: true, data });
+// //   } catch (error) {
+// //     console.log(error);
+// //     res.status(500).json({ ok: false, error });
+// //   }
+// // };
+
+// // const getListenCatById = async (req, res) => {
+// //   const result = await getListenCatByIdRaw(req.params.id);
+// //   if (result.ok) {
+// //     res.status(200).json(result);
+// //   } else {
+// //     res.status(500).json(result);
+// //   }
+// // };
+
+// // const getListenCatByIdRaw = async (id) => {
+// //   try {
+// //     const ref = doc(db, "categorie_listen", id);
+// //     const listencat = await getDoc(ref);
+// //     return { ok: true, data: { uid: listencat.id, ...listencat.data() } };
+// //   } catch (error) {
+// //     return { ok: false, error };
+// //   }
+// // };
+
+// // const deleteListenCatById = async (req, res) => {
+// //   try {
+// //     //console.log(req.body);
+// //     const ref = doc(db, "categorie_listen", req.body.id);
+
+// //     deleteDoc(ref)
+// //       .then((r) => {
+// //         //console.log(r);
+// //         res.status(200).json({ ok: true });
+// //       })
+// //       .catch((error) => {
+// //         res.status(500).json({ ok: false, error });
+// //       });
+// //   } catch (error) {
+// //     res.status(500).json({ ok: false, error });
+// //   }
+// // };
+
+// // const getbyid = async (req, res) => {
+// //   const { id } = req.params;
+// //   console.log(id);
+// //   let final;
+// //   const data = [];
+// //   let abonament = "";
+// //   try {
+// //     const [decodedToken, userId, user] = await check(req);
+// //     abonament = user.abonament;
+// //   } catch (error) {
+// //     abonament = false;
+// //   }
+// //   try {
+// //     const medCats = await getDocs(collection(db, "categorie_listen"));
+// //     medCats.forEach((doc) => {
+// //       data.push({ uid: doc.id, ...doc.data() });
+// //     });
+
+// //     for (let i = 0; i < data.length; i++) {
+// //       for (let j = 0; j < data[i].listenRoutines.length; j++) {
+// //         // if (data[i].listenRoutines.length > 0)
+// //         if (data[i].listenRoutines[j].id == id) {
+// //           if (abonament != false && abonament != "") {
+// //             data[i].listenRoutines[j].isLocked = false;
+// //           }
+// //           final = data[i].listenRoutines[j];
+// //           break;
+// //         } else {
+// //           data[i].listenRoutines.splice(j, 1);
+// //           j--;
+// //         }
+// //       }
+// //     }
+// //     //console.log(abonament);
+// //     // console.log(data);
+// //     console.log(final);
+// //     res.status(200).json({ data: { ...final } });
+// //   } catch (error) {
+// //     console.log(error);
+// //     res.status(500).json({ ok: false, error });
+// //   }
+// // };
+// // export {
+// //   addListenCat,
+// //   getAllListenCats,
+// //   getListenCatById,
+// //   deleteListenCatById,
+// //   getbyid,
+// //   getListenCatByIdRaw,
+// //   orderByField,
+// // };
 // import { db, storage } from "../../config_fire.js";
 // import {
 //   addDoc,
@@ -70,12 +270,22 @@
 // const getAllListenCats = async (req, res) => {
 //   let data = [];
 //   let abonament = "";
-//   try {
-//     const [decodedToken, userId, user] = await check(req);
-//     abonament = user.abonament;
-//   } catch (error) {
-//     abonament = false;
+//   let user = null;
+//   if (
+//     req.headers.authorization &&
+//     req.headers.authorization.startsWith("Bearer ")
+//   ) {
+//     try {
+//       const [decodedToken, userId, userObj] = await check(req);
+//       abonament = userObj.abonament;
+//       user = userObj;
+//     } catch (error) {
+//       abonament = "";
+//     }
+//   } else {
+//     abonament = ""; // user anonim
 //   }
+
 //   try {
 //     const listenCats = await getDocs(
 //       collection(db, "categorie_listen"),
@@ -84,15 +294,12 @@
 //     listenCats.forEach((doc) => {
 //       data.push({ uid: doc.id, ...doc.data() });
 //     });
-
 //     data = orderByField(data, "order", false);
-//     console.log(abonament);
-
-//     if (abonament != false && abonament != "") {
-//       for (let i = 0; i < data.length; i++) {
-//         for (let j = 0; j < data[i].listenRoutines.length; j++) {
-//           data[i].listenRoutines[j].isLocked = false;
-//         }
+//     for (let i = 0; i < data.length; i++) {
+//       for (let j = 0; j < data[i].listenRoutines.length; j++) {
+//         data[i].listenRoutines[j].isLocked = abonament
+//           ? false
+//           : data[i].listenRoutines[j].isLocked;
 //       }
 //     }
 //     for (let i = 0; i < data.length; i++) {
@@ -101,11 +308,9 @@
 //         "time",
 //         true
 //       );
-//       console.log(data[i].listenRoutines);
 //     }
 //     res.status(200).json({ ok: true, data });
 //   } catch (error) {
-//     console.log(error);
 //     res.status(500).json({ ok: false, error });
 //   }
 // };
@@ -149,29 +354,27 @@
 
 // const getbyid = async (req, res) => {
 //   const { id } = req.params;
-//   console.log(id);
 //   let final;
 //   const data = [];
 //   let abonament = "";
 //   try {
-//     const [decodedToken, userId, user] = await check(req);
-//     abonament = user.abonament;
+//     const [decodedToken, userId, userObj] = await check(req);
+//     abonament = userObj.abonament;
+//     if (!abonament || abonament === "") {
+//       return res.status(403).json({ error: "Premium required" });
+//     }
 //   } catch (error) {
-//     abonament = false;
+//     return res.status(401).json({ error: "Unauthorized" });
 //   }
 //   try {
 //     const medCats = await getDocs(collection(db, "categorie_listen"));
 //     medCats.forEach((doc) => {
 //       data.push({ uid: doc.id, ...doc.data() });
 //     });
-
 //     for (let i = 0; i < data.length; i++) {
 //       for (let j = 0; j < data[i].listenRoutines.length; j++) {
-//         // if (data[i].listenRoutines.length > 0)
 //         if (data[i].listenRoutines[j].id == id) {
-//           if (abonament != false && abonament != "") {
-//             data[i].listenRoutines[j].isLocked = false;
-//           }
+//           data[i].listenRoutines[j].isLocked = false;
 //           final = data[i].listenRoutines[j];
 //           break;
 //         } else {
@@ -180,12 +383,8 @@
 //         }
 //       }
 //     }
-//     //console.log(abonament);
-//     // console.log(data);
-//     console.log(final);
 //     res.status(200).json({ data: { ...final } });
 //   } catch (error) {
-//     console.log(error);
 //     res.status(500).json({ ok: false, error });
 //   }
 // };
@@ -285,7 +484,6 @@ const getAllListenCats = async (req, res) => {
   } else {
     abonament = ""; // user anonim
   }
-
   try {
     const listenCats = await getDocs(
       collection(db, "categorie_listen"),
--- src/utils/home.js
+++ src/utils/home.js
@@ -1,3 +1,287 @@
+// import { collection, getDocs, orderBy } from "firebase/firestore";
+// import { db } from "../../config_fire.js";
+// import { check } from "../auth/auth.js";
+// import { getMedCatByIdRaw } from "../meditations/categories.js";
+// import { getListenCatByIdRaw, orderByField } from "../listen/categories.js";
+
+// const getIt = async (
+//   req,
+//   colection,
+//   routines,
+//   title,
+//   link,
+//   free,
+//   isBig,
+//   isOrderd,
+//   limit = 0
+// ) => {
+//   try {
+//     const cats = await getDocs(collection(db, colection));
+//     let abonament = "";
+//     console.log(
+//       "home:",
+//       req.headers.authorization &&
+//         req.headers.authorization.startsWith("Bearer ")
+//     );
+//     if (
+//       req.headers.authorization != undefined &&
+//       req.headers.authorization.startsWith("Bearer ") != undefined
+//     ) {
+//       try {
+//         const [decodedToken, userId, userObj] = await check(req);
+//         console.log("decodedToken:", userId, userObj);
+//         abonament = userObj.abonament;
+//         // user = userObj;
+//       } catch (error) {
+//         abonament = "";
+//       }
+//     } else {
+//       abonament = ""; // user anonim
+//     }
+
+//     let items = [];
+//     let i = 0;
+//     cats.forEach((doc) => {
+//       doc.data()[routines].map((e) => {
+//         if (free) {
+//           if (
+//             // todo: for production
+//             !e.isLocked
+//           ) {
+//             items.push({
+//               title: e.title,
+//               background: e.background,
+//               linkTo: e[link],
+//               linkInfo: null,
+//               totalTime: Number(e.duration),
+//               isLocked: false,
+//               time: e.time,
+//             });
+//           }
+//         } else {
+//           if (i < limit)
+//             items.push({
+//               title: e.title,
+//               background: e.background,
+//               linkTo: e[link],
+//               linkInfo: null,
+//               totalTime: Number(e.duration),
+//               isLocked:
+//                 abonament != false && abonament != "" ? false : e.isLocked,
+//               time: e.time,
+//             });
+//         }
+//         i++;
+//       });
+//     });
+//     if (isOrderd) {
+//       items = items.sort((a, b) => b.time - a.time);
+//     }
+
+//     // if (abonament != false && abonament != "") {
+//     //   for (let i = 0; i < items.length; i++) {
+//     //     for (let j = 0; j < items[i][routines].length; j++) {
+//     //       items[i][routines][j].isLocked = false;
+//     //     }
+//     //   }
+//     // }
+
+//     return {
+//       title: title,
+//       isBig: isBig,
+//       items: items,
+//       meditationCategories: null,
+//       soundCategories: null,
+//     };
+//   } catch (error) {
+//     return false;
+//   }
+// };
+
+// const getInfos = async (req, cat, uid) => {
+//   let linkInfo = "";
+//   let items = [];
+//   let abonament = "";
+//   if (
+//     req.headers.authorization &&
+//     req.headers.authorization.startsWith("Bearer ")
+//   ) {
+//     try {
+//       const [decodedToken, userId, userObj] = await check(req);
+//       abonament = userObj.abonament;
+//       // user = userObj;
+//     } catch (error) {
+//       abonament = "";
+//     }
+//   } else {
+//     abonament = ""; // user anonim
+//   }
+
+//   if (cat === "categorie_meditati") {
+//     linkInfo = await getMedCatByIdRaw(uid);
+//     linkInfo = orderByField(linkInfo?.data?.meditationRoutines, "time", true);
+
+//     linkInfo.forEach((e) => {
+//       items.push({
+//         category: e.category,
+//         id: e.id,
+//         tags: e.tags,
+//         isLocked: abonament != false && abonament != "" ? false : e.isLocked,
+//         title: e.title,
+//         time: e.time,
+//         duration: e.duration.toString(),
+//         meditationLink: e.meditationLink,
+//         background: e.background,
+//       });
+//     });
+//   } else {
+//     linkInfo = await getListenCatByIdRaw(uid);
+//     linkInfo = orderByField(linkInfo?.data?.listenRoutines, "time", true);
+
+//     linkInfo.forEach((e) => {
+//       items.push({
+//         background: e.background,
+//         id: e.id,
+//         category: e.category,
+//         isLocked: abonament != false && abonament != "" ? false : e.isLocked,
+//         time: e.time,
+//         title: e.title,
+//         listenLink: e.listenLink,
+//         duration: e.duration.toString(),
+//       });
+//     });
+//   }
+//   console.log("abonament de pe home de la getInfos:", abonament);
+//   console.log("items:", items);
+//   // console.log("linkInfo:", linkInfo);
+//   return items;
+// };
+
+// const getCat = async (req, cat, title, isBig, type) => {
+//   try {
+//     let data = [];
+
+//     const medCats = await getDocs(
+//       collection(db, cat),
+//       orderBy("order", "desc")
+//     );
+
+//     let index = 0;
+
+//     medCats.forEach(async (doc, index) => {
+//       const el = doc.data();
+//       const uid = doc.id;
+
+//       data.push({
+//         id: doc.id,
+//         order: el.order,
+//         title: el.categoryTitle,
+//         background: el.backgroundImage,
+//         linkTo:
+//           cat === "categorie_meditati" ? "/meditation/item" : "/listen/item",
+//         // linkInfo: await getInfos(cat, uid),
+//         totalTime:
+//           el[
+//             cat === "categorie_meditati"
+//               ? "meditationRoutines"
+//               : "listenRoutines"
+//           ].length,
+//         isLocked: false,
+//       });
+//     });
+
+//     data = data.sort((a, b) => b.order - a.order);
+
+//     for (const d of data) {
+//       const infos = await getInfos(req, cat, d.id);
+//       d.linkInfo = infos;
+//     }
+
+//     return {
+//       title: title,
+//       isBig: isBig,
+//       meditationCategories: cat === "categorie_meditati" ? data : null,
+//       soundCategories: cat === "categorie_listen" ? data : null,
+//       items: null,
+//     };
+//   } catch (error) {
+//     console.log(error);
+//     return false;
+//   }
+// };
+
+// const getHome = async (req, res) => {
+//   const med_cat = await getCat(
+//     req,
+//     "categorie_meditati",
+//     "Colecții Meditații",
+//     true
+//   );
+//   const list_cat = await getCat(
+//     req,
+//     "categorie_listen",
+//     "Colecții Sunete",
+//     true
+//   );
+//   const free_meditations = await getIt(
+//     req,
+//     "categorie_meditati",
+//     "meditationRoutines",
+//     "Ascultă gratuit",
+//     "meditationLink",
+//     true,
+//     false,
+//     false
+//   );
+//   const last_meditations = await getIt(
+//     req,
+//     "categorie_meditati",
+//     "meditationRoutines",
+//     "Recent adăugate",
+//     "meditationLink",
+//     false,
+//     false,
+//     true,
+//     5
+//   );
+//   const last_listens = await getIt(
+//     req,
+//     "categorie_listen",
+//     "listenRoutines",
+//     "Recent adaugate",
+//     "listenLink",
+//     false,
+//     false,
+//     true,
+//     5
+//   );
+
+//   const data = [
+//     med_cat != false && med_cat,
+//     free_meditations != false && free_meditations,
+//     last_meditations != false && last_meditations,
+//     list_cat != false && list_cat,
+//     last_listens != false && last_listens,
+//   ];
+//   console.log(data);
+//   try {
+    
+//     const [decodedToken, userId, userObj] = await check(req);
+//     console.log("pt userul", userObj.email);
+//   } catch (e) {
+//     console.log("err la catch:", e);
+//   }
+
+//   if (data.length > 0) {
+//     console.log("e ok: ");
+//     res.status(200).json({ data });
+//   } else {
+//     console.log("nu e ok!");
+//     res.status(500).json({ ok: false });
+//   }
+// };
+
+// export { getHome };
 import { collection, getDocs, orderBy } from "firebase/firestore";
 import { db } from "../../config_fire.js";
 import { check } from "../auth/auth.js";
@@ -18,27 +302,17 @@ const getIt = async (
   try {
     const cats = await getDocs(collection(db, colection));
     let abonament = "";
-    console.log(
-      "home:",
-      req.headers.authorization &&
-        req.headers.authorization.startsWith("Bearer ")
-    );
-    if (
-      req.headers.authorization != undefined &&
-      req.headers.authorization.startsWith("Bearer ") != undefined
-    ) {
-      try {
-        const [decodedToken, userId, userObj] = await check(req);
-        console.log("decodedToken:", userId, userObj);
-        abonament = userObj.abonament;
-        // user = userObj;
-      } catch (error) {
-        abonament = "";
+    try {
+      const checkResult = await check(req);
+      if (Array.isArray(checkResult) && checkResult.length >= 3) {
+        const [decodedToken, userId, user] = checkResult;
+        abonament = user.abonament;
+      } else {
+        abonament = false;
       }
-    } else {
-      abonament = ""; // user anonim
+    } catch (error) {
+      abonament = false;
     }
-
     let items = [];
     let i = 0;
     cats.forEach((doc) => {
@@ -102,21 +376,17 @@ const getInfos = async (req, cat, uid) => {
   let linkInfo = "";
   let items = [];
   let abonament = "";
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer ")
-  ) {
-    try {
-      const [decodedToken, userId, userObj] = await check(req);
-      abonament = userObj.abonament;
-      // user = userObj;
-    } catch (error) {
-      abonament = "";
+  try {
+    const checkResult = await check(req);
+    if (Array.isArray(checkResult) && checkResult.length >= 3) {
+      const [decodedToken, userId, user] = checkResult;
+      abonament = user.abonament;
+    } else {
+      abonament = false;
     }
-  } else {
-    abonament = ""; // user anonim
+  } catch (error) {
+    abonament = false;
   }
-
   if (cat === "categorie_meditati") {
     linkInfo = await getMedCatByIdRaw(uid);
     linkInfo = orderByField(linkInfo?.data?.meditationRoutines, "time", true);
@@ -211,73 +481,68 @@ const getCat = async (req, cat, title, isBig, type) => {
 };
 
 const getHome = async (req, res) => {
-  const med_cat = await getCat(
-    req,
-    "categorie_meditati",
-    "Colecții Meditații",
-    true
-  );
-  const list_cat = await getCat(
-    req,
-    "categorie_listen",
-    "Colecții Sunete",
-    true
-  );
-  const free_meditations = await getIt(
-    req,
-    "categorie_meditati",
-    "meditationRoutines",
-    "Ascultă gratuit",
-    "meditationLink",
-    true,
-    false,
-    false
-  );
-  const last_meditations = await getIt(
-    req,
-    "categorie_meditati",
-    "meditationRoutines",
-    "Recent adăugate",
-    "meditationLink",
-    false,
-    false,
-    true,
-    5
-  );
-  const last_listens = await getIt(
-    req,
-    "categorie_listen",
-    "listenRoutines",
-    "Recent adaugate",
-    "listenLink",
-    false,
-    false,
-    true,
-    5
-  );
-
-  const data = [
-    med_cat != false && med_cat,
-    free_meditations != false && free_meditations,
-    last_meditations != false && last_meditations,
-    list_cat != false && list_cat,
-    last_listens != false && last_listens,
-  ];
-  console.log(data);
   try {
-    
-    const [decodedToken, userId, userObj] = await check(req);
-    console.log("pt userul", userObj.email);
-  } catch (e) {
-    console.log("err la catch:", e);
-  }
+    const med_cat = await getCat(
+      req,
+      "categorie_meditati",
+      "Colecții Meditații",
+      true
+    );
+    const list_cat = await getCat(
+      req,
+      "categorie_listen",
+      "Colecții Sunete",
+      true
+    );
+    const free_meditations = await getIt(
+      req,
+      "categorie_meditati",
+      "meditationRoutines",
+      "Ascultă gratuit",
+      "meditationLink",
+      true,
+      false,
+      false
+    );
+    const last_meditations = await getIt(
+      req,
+      "categorie_meditati",
+      "meditationRoutines",
+      "Recent adăugate",
+      "meditationLink",
+      false,
+      false,
+      true,
+      5
+    );
+    const last_listens = await getIt(
+      req,
+      "categorie_listen",
+      "listenRoutines",
+      "Recent adaugate",
+      "listenLink",
+      false,
+      false,
+      true,
+      5
+    );
 
-  if (data.length > 0) {
-    console.log("e ok: ");
-    res.status(200).json({ data });
-  } else {
-    console.log("nu e ok!");
-    res.status(500).json({ ok: false });
+    const data = [
+      med_cat,
+      free_meditations,
+      last_meditations,
+      list_cat,
+      last_listens,
+    ].filter(Boolean);
+
+    if (data.length > 0) {
+      res.status(200).json({ data });
+    } else {
+      res.status(500).json({ ok: false });
+    }
+  } catch (error) {
+    console.log("err la catch:", error);
+    res.status(500).json({ ok: false, error: error.message });
   }
 };
 
